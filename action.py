6fc3681e4d53d6e1b9cca0b5b7bbd918f61e0223
Jacky <jlian5@ocdsb.ca>
2018-04-17 09:46:06 -0400

    Working On sprite

--- action.py
+++ action.py
@@ -1,21 +1,58 @@
 # ICS3U
 # Assignment 2: Action
-# <your name>
+# Jacky Liang
 
 # adapted from http://www.101computing.net/getting-started-with-pygame/
 
 # Import the pygame library and initialise the game engine
 # Don't forget to import your class
-import pygame
+import pygame, random
 pygame.init()
+
+speed = random.randint (5, 10)
 
 # Define some colours
 # Colours are defined using RGB values
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
+# Space ship sprite
+class SpaceShip (pygame.sprite.Sprite):
+
+    def __init__ (self, c, w, h, v) :
+        super ().__init__()
+
+        self.image = pygame.Surface ([w+100, h+100])
+        self.image.fill (WHITE)
+        self.image.set_colorkey (WHITE)
+
+        self.width = w
+        self.height = h
+        self.color = c
+        self.speed = v
+
+        # Draw Space ship (cockpit with 2 thrusters)
+        pygame.draw.rect (self.image, c, [25, 50, w, h])
+        pygame.draw.rect (self.image, RED, [15, 25, w, h//2])
+        pygame.draw.rect (self.image, BLUE, [15, 75, w, h//2])
+
+        self.rect = self.image.get_rect ()
+
+    def moveRight (self, speed) :
+        self.rect.x += self.speed * speed/20
+
+    def changeSpeed (self, speed) :
+        self.speed = speed
+
+    def repaint (self, color) :
+        self.color = color
+        pygame.draw.rect (self.image, self.color, [0, 0, self.width, self.height])
+        pygame.draw.rect (self.image, self.color, [-25, 25, self.width, self.height])
+        pygame.draw.rect (self.image, self.color, [-25, -25, self.width, self.height])
+            
 # Set the screen size
 SCREENWIDTH = 400
 SCREENHEIGHT = 400
@@ -25,6 +62,14 @@ SCREENHEIGHT = 400
 size = (SCREENWIDTH, SCREENHEIGHT)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My Animation")
+
+spriteList = pygame.sprite.Group ()
+
+ship1 = SpaceShip (GREEN, 55, 25, 20)
+ship1.rect.x = 200
+ship1.rect.y = SCREENHEIGHT // 2
+
+spriteList.add (ship1)
 
 # This loop will continue until the user exits the game
 carryOn = True
@@ -39,24 +84,23 @@ while carryOn:
         if event.type == pygame.QUIT: # Player clicked close button
             carryOn = False
 
+        
     # --- Game logic goes here
     # There should be none for a static image
     
     # --- Draw code goes here
 
     # Clear the screen to white
-    screen.fill(WHITE)
+    screen.fill (WHITE)
 
     # Queue different shapes and lines to be drawn
-    # pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
-    # pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    # pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
+    spriteList.draw (screen)
 
     # Update the screen with queued shapes
-    pygame.display.flip()
+    pygame.display.flip ()
 
     # --- Limit to 60 frames per second
-    clock.tick(60)
+    clock.tick (60)
 
 # Once the main program loop is exited, stop the game engine
-pygame.quit()
+pygame.quit ()
